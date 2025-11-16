f0591e7dbcdb71c585d0a6e771dcf01361c8552d
Vishruth M V <113712071+VMV09@users.noreply.github.com>
2025-11-16 21:31:30 +0530

    Update HelloWorld test cases for new outputs

--- tests/test_HelloWorld.py
+++ tests/test_HelloWorld.py
@@ -1,4 +1,6 @@
 from basic_programs.HelloWorld import helloworld
 
 def test_helloworld():
-  assert helloworld() == "Hello World"
+  assert helloworld() == "Hello World, Python"
+  assert helloworld(1) == "Hello World"
+  assert helloworld(2) == "Hello Python"
